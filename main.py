988962b400f0168bb35917d8678c13ca583d1976
Darshil Sharma <darshil.ips@gmail.com>
2026-02-05 21:06:06 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,49 @@
+from turtle import Turtle,Screen
+from paddle import Paddle
+from ball import Ball
+import time
+
+from scoreboard import Scoreboard
+screen = Screen()
+
+screen.bgcolor("Gray90")
+screen.title("Pong Ball game")
+screen.setup(width =800 , height =500 )
+screen.tracer(2)
+
+
+r_paddle = Paddle((350,0))
+l_paddle = Paddle((-350,0))
+
+
+
+
+ball = Ball()
+screen.listen()
+scoreboard = Scoreboard()
+
+screen.onkey(r_paddle.go_up,"Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up,"w")
+screen.onkey(l_paddle.go_down, "s")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+    if ball.ycor() > 230 or ball.ycor() <-230:
+        ball.bounce_y()
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50  and ball.xcor() < -320:
+        ball.bounce_x()
+    if ball.xcor() > 390:
+        scoreboard.l_point()
+        ball.reset_position()
+
+
+    if ball.xcor() < -390:
+        scoreboard.r_point()
+        ball.reset_position()
+
+
+screen.exitonclick()
